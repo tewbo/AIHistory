2426c64447d0a62d7869ab3b9b2bb2e418444958
tewbo <shotgun1317@gmail.com>
2023-12-14 15:49:04 +0300

    + add prompts and pretty view

--- app/static/script.js
+++ app/static/script.js
@@ -1,3 +1,28 @@
+// Добавляем функцию для выбора варианта
+function selectOption(option) {
+    var chatBox = document.getElementById('chat-box');
+    chatBox.innerHTML += '<div class="system-message">' + 'Вселенная: Вы выбрали персонажа <i>' + option + '</i></div>';
+
+    // Отправляем выбранный вариант на сервер для обработки
+    fetch('/processOption', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: 'option=' + option,
+    })
+    .then(response => response.text())
+    .then(data => {
+        // Отображаем анимацию с изображением в левой части страницы
+        var animationContainer = document.getElementById('animation-container');
+        animationContainer.innerHTML = `<img id="animation" src="static/images/` + data + `" width=" 300" height="402">`
+
+        // Очищаем поле ввода
+        document.getElementById('user-input').value = '';
+    });
+}
+
+// Добавляем функцию для отправки сообщения пользователя
 function sendMessage() {
     var userInput = document.getElementById('user-input').value;
 
